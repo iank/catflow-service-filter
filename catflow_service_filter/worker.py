88f29231f0fcbb779f270fdf716621f3b7cc5ea4
Ian Kilgore <iank@iank.org>
2023-06-28 20:47:32 -0400

    VDB filter service

--- catflow_service_filter/worker.py
+++ catflow_service_filter/worker.py
@@ -2,17 +2,102 @@ from typing import Any, List, Tuple
 import signal
 import asyncio
 from catflow_worker import Worker
+from catflow_worker.types import (
+    Embedding,
+    RawFrameSchema,
+    EmbeddedFrameSchema,
+    RawFrame,
+)
+import numpy as np
+import psycopg2
+import os
+from pgvector.psycopg2 import register_vector
 
 import logging
 
 
-async def filter_handler(
-    msg: Any, key: str, s3: Any, bucket: str
-) -> Tuple[bool, List[Tuple[str, Tuple[str, List[float]]]]]:
-    """Run filter on the given frames"""
-    logging.info(f"[*] Message received ({key})")
+class VDB:
+    """DB ops"""
 
-    return True, [("filter.rawframes", [])]
+    def __init__(self, pgconfig, threshold):
+        self.threshold = threshold
+
+        self.db = psycopg2.connect(
+            host=pgconfig["host"],
+            dbname=pgconfig["dbname"],
+            user=pgconfig["username"],
+            password=pgconfig["password"],
+            port=pgconfig["port"],
+        )
+        register_vector(self.db)
+
+    def check_novel(self, embedding: Embedding):
+        embedding = np.array(embedding.vector)
+
+        # Check nearest
+        query = (
+            "SELECT uuid, cosine_distance(embedding, %s) as distance"
+            " FROM images ORDER BY distance LIMIT 1"
+        )
+        cur = self.db.cursor()
+        cur.execute(query, (embedding,))
+        results = cur.fetchall()
+        cur.close()
+
+        if len(results) == 0:
+            return True
+
+        (nearest_uuid, min_distance) = results[0]  # first/only result
+        logging.debug(
+            f"cosine distance to {nearest_uuid} (nearest): {min_distance:.4f}"
+        )
+        if min_distance < self.threshold:
+            return False
+
+        return True
+
+    def add(self, uuid: str, source: str, embedding: Embedding):
+        embedding = np.array(embedding.vector)
+        cur = self.db.cursor()
+        insertQuery = "INSERT INTO images (uuid, embedding, source) VALUES (%s, %s, %s)"
+        cur.execute(insertQuery, (uuid, embedding, source))
+        self.db.commit()
+        cur.close()
+
+
+def create_filter_handler(db):
+    async def _filter_handler(
+        msg: Any, key: str, s3: Any, bucket: str
+    ) -> Tuple[bool, List[Tuple[str, Any]]]:
+        """Run filter on the given frames"""
+        logging.info(f"[*] Message received ({key})")
+
+        # Load message
+        embedded_frames = EmbeddedFrameSchema(many=True).load(msg)
+
+        # Check frames and add them to DB one by one
+        responseobjects = []
+        for frame in embedded_frames:
+            embedding = frame.embedding
+
+            if db.check_novel(embedding):
+                db.add(frame.key, frame.source.key, embedding)
+                rawframe = RawFrame(key=frame.key, source=frame.source)
+                logging.info(f"[-] Adding {rawframe!s} to database")
+                responseobjects.append(rawframe)
+
+        # Empty response if no frames added
+        if len(responseobjects) == 0:
+            return True, []
+
+        # Dump response
+        schema_out = RawFrameSchema(many=True)
+        responseobjects_msg = schema_out.dump(responseobjects)
+
+        logging.info(f"[-] {len(responseobjects)} objects -> filter.rawframes (1 msg)")
+        return True, [("filter.rawframes", responseobjects_msg)]
+
+    return _filter_handler
 
 
 async def shutdown(worker, task):
@@ -25,6 +110,21 @@ async def shutdown(worker, task):
 
 
 async def startup(queue: str, topic_key: str):
+    # Set up DB
+    pgconfig = {
+        "host": os.environ["CATFLOW_PG_HOST"],
+        "dbname": os.environ["CATFLOW_PG_DBNAME"],
+        "username": os.environ["CATFLOW_PG_USER"],
+        "password": os.environ["CATFLOW_PG_PASSWORD"],
+        "port": int(os.environ["CATFLOW_PG_PORT"]),
+    }
+
+    threshold = float(os.environ["CATFLOW_VDB_THRESHOLD"])
+
+    db = VDB(pgconfig, threshold)
+    filter_handler = create_filter_handler(db)
+
+    # Start worker
     worker = await Worker.create(filter_handler, queue, topic_key)
     task = asyncio.create_task(worker.work())
 
--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,9 +1,144 @@
 import pytest
-from catflow_service_filter.worker import filter_handler
+from catflow_service_filter.worker import create_filter_handler
+from catflow_worker.types import (
+    Embedding,
+    EmbeddedFrame,
+    EmbeddedFrameSchema,
+    RawFrameSchema,
+    VideoFile,
+)
+import numpy as np
+
+
+@pytest.fixture()
+def vdb():
+    return VDB_Mock(threshold=0.2)
+
+
+class VDB_Mock:
+    """Fake DB ops rather than stand up a pgvector DB for unit tests"""
+
+    def __init__(self, threshold):
+        self.threshold = threshold
+        self.database = {}
+
+    def cosine_distance(self, a, b):
+        return 1 - np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+    def check_novel(self, embedding: Embedding):
+        # filter
+        distances = [
+            self.cosine_distance(x, embedding.vector) for x in self.database.values()
+        ]
+        if len(distances) == 0:
+            return True
+
+        closest = np.min(distances)
+        if closest < self.threshold:
+            return False
+        else:
+            return True
+
+    def add(self, uuid: str, source: str, embedding: Embedding):
+        self.database[uuid] = embedding.vector
 
 
 @pytest.mark.asyncio
-async def test_worker():
-    # Test worker's behavior
-    filter_handler()  # TODO
-    pass
+async def test_worker_correct_output(vdb):
+    filter_handler = create_filter_handler(vdb)
+
+    # Expected input: list of EmbeddedFrame
+    video = VideoFile(key="test.mp4")
+    frames = [
+        EmbeddedFrame(
+            key="test1.png", source=video, embedding=Embedding(vector=[1.0, -1.0, 3])
+        ),
+    ]
+    embeddings_msg = EmbeddedFrameSchema(many=True).dump(frames)
+
+    # Expected output: list of RawFrame
+    output_schema = RawFrameSchema(many=True)
+
+    status, responses = await filter_handler(
+        embeddings_msg, "filter.embeddings", None, None
+    )
+
+    assert status is True
+    assert len(responses) == 1
+
+    routing_key, frames_msg = responses[0]
+    frames = output_schema.load(frames_msg)
+
+    assert routing_key == "filter.rawframes"
+    assert len(frames) == 1
+
+    frame = frames[0]
+    assert frame.key == "test1.png"
+    assert frame.source.key == "test.mp4"
+
+
+@pytest.mark.asyncio
+async def test_worker_reject_close_embedding(vdb):
+    filter_handler = create_filter_handler(vdb)
+
+    # Add an embedding to DB
+    vdb.add("test.png", "", Embedding(vector=[1.0, -1.0, 3.1]))
+
+    # Expected input: list of EmbeddedFrame
+    video = VideoFile(key="test.mp4")
+    frames = [
+        EmbeddedFrame(
+            key="tooclose.png", source=video, embedding=Embedding(vector=[1.0, -1.0, 3])
+        ),
+    ]
+    embeddings_msg = EmbeddedFrameSchema(many=True).dump(frames)
+
+    status, responses = await filter_handler(
+        embeddings_msg, "filter.embeddings", None, None
+    )
+
+    # test that close embedding gets rejected
+    assert status is True
+    assert len(responses) == 0
+
+
+@pytest.mark.asyncio
+async def test_worker_add_far_embedding(vdb):
+    filter_handler = create_filter_handler(vdb)
+
+    # Add an embedding to DB
+    vdb.add("test.png", "", Embedding(vector=[1.0, -1.0, 3.1]))
+
+    # Expected input: list of EmbeddedFrame
+    video = VideoFile(key="test.mp4")
+    frames = [
+        EmbeddedFrame(
+            key="novel.png", source=video, embedding=Embedding(vector=[1.0, 1.0, 1.0])
+        ),
+        EmbeddedFrame(
+            key="similar_to_the_first_one.png",
+            source=video,
+            embedding=Embedding(vector=[1.0, 1.0, 1.1]),
+        ),
+    ]
+    embeddings_msg = EmbeddedFrameSchema(many=True).dump(frames)
+
+    # Expected output: list of RawFrame
+    output_schema = RawFrameSchema(many=True)
+
+    status, responses = await filter_handler(
+        embeddings_msg, "filter.embeddings", None, None
+    )
+
+    assert status is True
+    assert len(responses) == 1
+
+    routing_key, frames_msg = responses[0]
+    frames = output_schema.load(frames_msg)
+
+    # Test that the first frame was added but not the second
+    assert frames[0].key == "novel.png"
+
+    # verify that it was actually added to DB
+    assert "novel.png" in vdb.database
+    assert "similar_to_the_first_one" not in vdb.database
